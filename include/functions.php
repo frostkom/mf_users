3ec9c14f8c342f10b43eb0ac9b0d9d72a08dcdb6
frostkom <martin.fors@frostkom.se>
2016-01-02 12:10:11 +0100

    First commit

---
+++ include/functions.php
@@ -0,0 +1,294 @@
+<?php 
+
+if(!function_exists('wp_authenticate'))
+{
+	function wp_authenticate($username, $password)
+	{
+		$username = sanitize_user($username);
+		$password = trim($password);
+
+		//This is the extra line for replacing spaces in the username
+		if(get_option('setting_users_no_spaces'))
+		{
+			$username = replace_spaces($username);
+		}
+
+		$user = apply_filters('authenticate', null, $username, $password);
+
+		if($user == null)
+		{
+			$user = new WP_Error('authentication_failed', __('Incorrect username or password', 'lang_users'));
+		}
+
+		$ignore_codes = array('empty_username', 'empty_password');
+
+		if(is_wp_error($user) && !in_array($user->get_error_code(), $ignore_codes))
+		{
+			do_action('wp_login_failed', $username);
+		}
+
+		return $user;
+	}
+}
+
+function replace_spaces($in)
+{
+	return str_replace(" ", "-", $in);
+}
+
+function register_form_users()
+{
+	$first_name = check_var('first_name');
+	$last_name = check_var('last_name');
+
+	echo "<p>
+		<label for='first_name'>".__('First Name', 'lang_users').":</label><br>
+		<input type='text' name='first_name' value='".$first_name."' class='regular-text' required>
+	</p>
+	<p>
+		<label for='last_name'>".__('Last Name', 'lang_users').":</label><br>
+		<input type='text' name='last_name' value='".$last_name."' class='regular-text' required>
+	</p>";
+}
+
+function user_register_users($user_id, $password = "", $meta = array())
+{
+	$first_name = check_var('first_name');
+	$last_name = check_var('last_name');
+
+	update_usermeta($user_id, 'first_name', $first_name);
+	update_usermeta($user_id, 'last_name', $last_name);
+}
+
+function register_errors_users($errors, $user_login, $user_email)
+{
+	if(preg_match('/ /', $user_login))
+	{
+		$errors->add('user_login', __("Username must not contain spaces", 'lang_users'));
+	}
+	
+	return $errors;
+}
+
+function save_display_name($user)
+{
+	if($user->first_name != '' && $user->last_name != '')
+	{
+		$display_name = $user->first_name." ".$user->last_name;
+
+		if($user->display_name != $display_name)
+		{
+			wp_update_user(array('ID' => $user->ID, 'display_name' => $display_name));
+		}
+	}
+}
+
+function wp_login_users($username)
+{
+	$user = get_user_by('login', $username);
+
+	save_display_name($user);
+
+	/*if($user->first_name != '' && $user->last_name != '')
+	{
+		$display_name = $user->first_name." ".$user->last_name;
+
+		if($user->data->display_name != $display_name)
+		{
+			wp_update_user(array('ID' => $user->ID, 'display_name' => $display_name));
+		}
+	}*/
+}
+
+function admin_head_users()
+{
+	$users = get_users(array('fields' => 'all'));
+
+	foreach($users as $user)
+	{
+		$user = get_userdata($user->ID);
+
+		save_display_name($user);
+
+		/*if($user->first_name != '' && $user->last_name != '')
+		{
+			$display_name = $user->first_name." ".$user->last_name;
+
+			if($user->display_name != $display_name)
+			{
+				wp_update_user(array('ID' => $user->ID, 'display_name' => $display_name));
+			}
+		}*/
+	}
+}
+
+/*function init_users()
+{
+	global $wp_roles;
+
+	if(!isset($wp_roles))
+	{
+		$wp_roles = new WP_Roles();
+	}
+
+	$wp_user_roles_orig = get_option('wp_user_roles_orig');
+
+	if($wp_user_roles_orig == '')
+	{
+		$wp_user_roles_orig = get_option('wp_user_roles');
+
+		update_option('wp_user_roles_orig', $wp_user_roles_orig);
+	}
+
+	$option = get_option('setting_users_roles_names');
+
+	if(is_array($option))
+	{
+		foreach($option as $key => $value)
+		{
+			if($value != '')
+			{
+				$wp_roles->roles[$key]['name'] = $wp_roles->role_names[$key] = $value;
+
+				$wp_user_roles_orig[$key]['name'] = $value;
+			}
+		}
+	}
+
+	$option = get_option('setting_users_roles_hidden');
+
+	if(is_array($option))
+	{
+		foreach($option as $key => $value)
+		{
+			unset($wp_roles->roles[$key]);
+			unset($wp_roles->role_names[$key]);
+
+			unset($wp_user_roles_orig[$key]);
+		}
+	}
+
+	update_option('wp_user_roles', $wp_user_roles_orig);
+}*/
+
+function settings_users()
+{
+	$options_page = "settings_mf_base";
+	$options_area = "settings_users";
+
+	add_settings_section(
+		$options_area,
+		"",
+		$options_area."_callback",
+		$options_page
+	);
+
+	$arr_settings = array(
+		"setting_users_roles_hidden" => __("Hide Roles", 'lang_users'),
+		"setting_users_roles_names" => __("Change Role Names", 'lang_users'),
+		"setting_users_register_name" => __("Collect name of user in registration form", 'lang_users'),
+		"setting_users_no_spaces" => __("Prevent Username Spaces", 'lang_users'),
+	);
+
+	foreach($arr_settings as $handle => $text)
+	{
+		add_settings_field($handle, $text, $handle."_callback", $options_page, $options_area);
+
+		register_setting($options_page, $handle);
+	}
+}
+
+function settings_users_callback()
+{
+	echo "<div id='settings_users'>&nbsp;</div>
+	<a href='#settings_users'><h3>".__("Users", 'lang_users')."</h3></a>";
+
+	//Updates DB with custom roles
+	$wp_user_roles_orig = get_option('wp_user_roles_orig');
+
+	if($wp_user_roles_orig == '')
+	{
+		$wp_user_roles_orig = get_option('wp_user_roles');
+
+		update_option('wp_user_roles_orig', $wp_user_roles_orig);
+	}
+
+	$option = get_option('setting_users_roles_names');
+
+	if(is_array($option))
+	{
+		foreach($option as $key => $value)
+		{
+			if($value != '')
+			{
+				$wp_roles->roles[$key]['name'] = $wp_roles->role_names[$key] = $value;
+
+				$wp_user_roles_orig[$key]['name'] = $value;
+			}
+		}
+	}
+
+	$option = get_option('setting_users_roles_hidden');
+
+	if(is_array($option))
+	{
+		foreach($option as $key => $value)
+		{
+			unset($wp_roles->roles[$key]);
+			unset($wp_roles->role_names[$key]);
+
+			unset($wp_user_roles_orig[$key]);
+		}
+	}
+
+	update_option('wp_user_roles', $wp_user_roles_orig);
+}
+
+function setting_users_roles_hidden_callback()
+{
+	$option = get_option('setting_users_roles_hidden');
+
+	$roles = get_all_roles(array('orig' => true));
+
+	foreach($roles as $key => $value)
+	{
+		$option_value = isset($option[$key]) ? $option[$key] : "";
+
+		echo show_checkbox(array('name' => "setting_users_roles_hidden[".$key."]", 'text' => __($value), 'value' => 1, 'compare' => $option_value));
+	}
+}
+
+function setting_users_roles_names_callback()
+{
+	$option = get_option('setting_users_roles_names');
+
+	$roles = get_all_roles();
+
+	foreach($roles as $key => $value)
+	{
+		$option_value = isset($option[$key]) ? $option[$key] : "";
+
+		echo "<p>"
+			.show_textfield(array('name' => "setting_users_roles_names[".$key."]", 'value' => $option_value, 'placeholder' => __($value)))
+			."<span class='description'>".__("Change name for", 'lang_users')." ".__($value)."</span>
+		</p>";
+	}
+}
+
+function setting_users_no_spaces_callback()
+{
+	$option = get_option('setting_users_no_spaces');
+
+	echo "<label>
+		<input type='checkbox' name='setting_users_no_spaces' value='1' ".checked(1, $option, false).">
+	</label>";
+}
+
+function setting_users_register_name_callback()
+{
+	$option = get_option('setting_users_register_name');
+
+	echo "<label>
+		<input type='checkbox' name='setting_users_register_name' value='1' ".checked(1, $option, false).">
+	</label>";
+}
---
+++ index.php
@@ -0,0 +1,80 @@
+<?php 
+/*
+Plugin Name: MF Users
+Plugin URI: 
+Description: 
+Version: 1.2.2
+Author: Martin Fors
+Author URI: http://frostkom.se
+*/
+
+include_once("include/functions.php");
+
+if(is_admin())
+{
+	register_activation_hook(__FILE__, 'activate_users');
+	register_deactivation_hook(__FILE__, 'deactivate_users');
+
+	//add_action('init', 'init_users', 1);
+	add_action('admin_init', 'settings_users');
+}
+
+else
+{
+	if(get_option('setting_users_register_name'))
+	{
+		add_action('register_form', 'register_form_users');
+		add_action('user_register', 'user_register_users');
+	}
+
+	if(get_option('setting_users_no_spaces'))
+	{
+		add_action('registration_errors', 'register_errors_users', 10, 3);
+	}
+}
+
+if(get_option('setting_users_register_name'))
+{
+	add_action('wp_login', 'wp_login_users');
+}
+
+load_plugin_textdomain('lang_users', false, dirname(plugin_basename(__FILE__)).'/lang/');
+
+function activate_users()
+{
+	global $wpdb;
+
+	if(get_option('setting_users_no_spaces'))
+	{
+		$users = get_users(array('fields' => 'all'));
+
+		foreach($users as $user)
+		{
+			$user_data = get_userdata($user->ID);    
+
+			$username = replace_spaces($user_data->user_login);
+
+			if($username != $user_data->user_login)
+			{
+				//wp_update_user(array('ID' => $user->ID, 'user_login' => $username)); //Does not work
+				$wpdb->query($wpdb->prepare("UPDATE ".$wpdb->users." SET user_login = %s WHERE ID = '%d'", $username, $user->ID));
+			}
+		}
+	}
+
+	if(get_option('setting_users_register_name'))
+	{
+		add_action('admin_head', 'admin_head_users');
+	}
+}
+
+function deactivate_users()
+{
+	$wp_user_roles_orig = get_option('wp_user_roles_orig');
+
+	if($wp_user_roles_orig != '')
+	{
+		update_option('wp_user_roles', $wp_user_roles_orig);
+		delete_option('wp_user_roles_orig');
+	}
+}
