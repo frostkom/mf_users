ef1ebcac34553eaa85539d40f1ae3b8c49eeb8d7
frostkom <martin.fors@frostkom.se>
2018-01-22 14:39:33 +0100

    Add licence

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 Plugin Name: MF Users
 Plugin URI: https://github.com/frostkom/mf_users
 Description: 
-Version: 4.1.1
+Version: 4.1.2
+Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_users
