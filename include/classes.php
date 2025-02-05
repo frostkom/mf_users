8eb1a7d2a110c80a71bc4620c250e6a1794e44f8
frostkom <martin.fors@frostkom.se>
2025-02-05 10:26:58 +0100

    Make script load in footer

--- include/classes.php
+++ include/classes.php
@@ -334,7 +334,7 @@ class mf_users
 		$plugin_include_url = plugin_dir_url(__FILE__);
 		$plugin_version = get_plugin_version(__FILE__);
 
-		wp_register_script('script_users_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version);
+		wp_register_script('script_users_block_wp', $plugin_include_url."block/script_wp.js", array('wp-blocks', 'wp-element', 'wp-components', 'wp-editor', 'wp-block-editor'), $plugin_version, true);
 
 		wp_localize_script('script_users_block_wp', 'script_users_block_wp', array(
 			'block_title' => __("User", 'lang_users'),
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Users
 Plugin URI: https://github.com/frostkom/mf_users
 Description:
-Version: 4.7.9
+Version: 4.7.10
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
